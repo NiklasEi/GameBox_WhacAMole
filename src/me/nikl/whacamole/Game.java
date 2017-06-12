76c73a9e7992288b62eff01343c798892f57f127
Niklas Eicker <nikle1@gmx.de>
2017-06-12 16:15:41 +0200

    first fluently running version with two gamemodes

--- src/me/nikl/whacamole/Game.java
+++ src/me/nikl/whacamole/Game.java
@@ -14,6 +14,7 @@ import org.bukkit.material.MaterialData;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Random;
+import java.util.logging.Level;
 
 /**
  * Created by Niklas on 14.04.2017.
@@ -36,7 +37,7 @@ public class Game extends BukkitRunnable {
 
     private Random random;
 
-    private int creeper = -99, human = -99;
+    private int moleSlot = -99, humanSlot = -99;
 
     private Sounds gameOver = Sounds.ANVIL_LAND;
 
@@ -44,7 +45,17 @@ public class Game extends BukkitRunnable {
 
     private int score = 0;
 
-    private ItemStack skull, cover, head;
+    private int time;
+
+    private boolean decreaseTime = false;
+
+    private ItemStack mole, grass, human, cover;
+
+    private boolean spawnHuman = false;
+
+    private int[] spawnLocations;
+
+    private GameState gameState = GameState.START;
 
 
     public Game(GameRules rule, Main plugin, Player player, boolean playSounds){
@@ -54,17 +65,24 @@ public class Game extends BukkitRunnable {
         this.player = player;
         this.random = new Random(System.currentTimeMillis());
 
-        skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.CREEPER.ordinal());
-        ItemMeta meta = skull.getItemMeta();
+        this.time = rule.getTime();
+
+        mole = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.CREEPER.ordinal());
+        ItemMeta meta = mole.getItemMeta();
         meta.setDisplayName("" + ChatColor.RED + ChatColor.BOLD + "Creeper");
-        skull.setItemMeta(meta);
+        mole.setItemMeta(meta);
 
-        head = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
-        meta = head.getItemMeta();
+        human = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+        meta = human.getItemMeta();
         meta.setDisplayName("" + ChatColor.GREEN + ChatColor.BOLD + "Human");
-        head.setItemMeta(meta);
+        human.setItemMeta(meta);
 
-        cover = new MaterialData(Material.LONG_GRASS).toItemStack(1);
+        grass = new MaterialData(Material.LONG_GRASS).toItemStack(1);
+        meta = grass.getItemMeta();
+        meta.setDisplayName(" ");
+        grass.setItemMeta(meta);
+
+        cover = new MaterialData(Material.STAINED_GLASS_PANE).toItemStack(1);
         meta = cover.getItemMeta();
         meta.setDisplayName(" ");
         cover.setItemMeta(meta);
@@ -73,7 +91,9 @@ public class Game extends BukkitRunnable {
         this.playSounds = plugin.getPlaySounds() && playSounds;
 
         // create inventory
-        this.inventory = Bukkit.createInventory(null, 54, lang.GAME_TITLE.replace("%score%", String.valueOf(score)));
+        this.inventory = Bukkit.createInventory(null, 54, lang.GAME_TITLE_START.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+
+        if(rule.getGameMode() == GameRules.GameMode.FULLINVENTORY) spawnHuman = true;
 
         buildInv();
 
@@ -84,49 +104,117 @@ public class Game extends BukkitRunnable {
     }
 
     private void buildInv() {
-        for(int i = 0 ; i < inventory.getSize(); i++){
-            inventory.setItem(i, cover);
+        switch (rule.getGameMode()) {
+            case FULLINVENTORY:
+                spawnLocations = new int[inventory.getSize()];
+                for (int i = 0; i < inventory.getSize(); i++) {
+                    inventory.setItem(i, grass);
+                    spawnLocations[i] = i;
+                }
+                break;
+
+            case CLASSIC:
+                spawnLocations = new int[4];
+                int counter = 0;
+                for (int i = 0; i < inventory.getSize(); i++) {
+                    if(i == 11 || i == 29 || i == 13 || i == 31){
+                        spawnLocations[counter] = i;
+                        counter++;
+                    } else {
+                        inventory.setItem(i, cover);
+                    }
+                }
+                break;
+
+            default:
+                plugin.getLogger().log(Level.SEVERE, " Unknown WAM GameMode");
+                break;
         }
     }
 
 
 
     public void onClick(InventoryClickEvent inventoryClickEvent) {
-        if(inventoryClickEvent.getRawSlot() != creeper) return;
+        if(this.gameState == GameState.GAME_OVER) return;
 
-        score++;
-        inventory.setItem(creeper, cover);
-        plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE.replace("%score%", String.valueOf(score)));
-        creeper = -99;
+        if(this.gameState == GameState.START){
+            gameState = GameState.PLAY;
+            plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+            return;
+        }
+
+        if(inventoryClickEvent.getRawSlot() != moleSlot && inventoryClickEvent.getRawSlot() != humanSlot) return;
+
+        switch (rule.getGameMode()){
+            case FULLINVENTORY:
+                if(inventoryClickEvent.getRawSlot() == moleSlot) {
+                    score++;
+                    inventory.setItem(moleSlot, grass);
+                    plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+                    moleSlot = -99;
+                } else {
+                    // human was hit
+                    if(rule.isGameOverOnHittingHuman()){
+                        gameState = GameState.GAME_OVER;
+                        plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE_LOST.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+                    } else {
+                        score = Math.max(score - rule.getPunishmentOnHittingHuman(), 0);
+                        plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+                    }
+                }
+
+                return;
+
+            case CLASSIC:
+                score++;
+                inventory.setItem(moleSlot, null);
+                plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+                moleSlot = -99;
+
+                return;
+        }
     }
 
     @Override
     public void run() {
-        Bukkit.getConsoleSender().sendMessage("running...");
-        if(creeper < 0){
-            if(random.nextDouble() > 0.5){
-                creeper = random.nextInt(inventory.getSize());
-                inventory.setItem(creeper, skull);
+        if(this.gameState != GameState.PLAY) return;
+
+        if(decreaseTime) time --;
+        decreaseTime = !decreaseTime;
+        if(time < 1){
+            this.gameState = GameState.GAME_OVER;
+            plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE_LOST.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+            return;
+        }
+        plugin.getNms().updateInventoryTitle(player, lang.GAME_TITLE.replace("%score%", String.valueOf(score)).replace("%time%", String.valueOf(time)));
+
+
+        if (moleSlot < 0) {
+            if (random.nextDouble() > 0.5) {
+                moleSlot = spawnLocations[random.nextInt(spawnLocations.length)];
+                inventory.setItem(moleSlot, mole);
             }
         } else {
-            if(random.nextDouble() > 0.5){
-                inventory.setItem(creeper, cover);
-                creeper = - 99;
+            if (random.nextDouble() < 0.4) {
+                inventory.setItem(moleSlot, rule.getGameMode() == GameRules.GameMode.FULLINVENTORY? grass : null);
+                moleSlot = -99;
             }
         }
 
-        if(human < 0){
-            if(random.nextDouble() > 0.9){
-                human = random.nextInt(inventory.getSize());
-                while (human == creeper){
-                    human = random.nextInt(inventory.getSize());
+        if (spawnHuman) {
+            if (humanSlot < 0) {
+                if (random.nextDouble() < 0.1) {
+                    humanSlot = spawnLocations[random.nextInt(spawnLocations.length)];
+                    while (humanSlot == moleSlot) {
+                        humanSlot = spawnLocations[random.nextInt(spawnLocations.length)];
+                    }
+                    inventory.setItem(humanSlot, human);
                 }
-                inventory.setItem(human, head);
-            }
-        } else {
-            if(random.nextDouble() > 0.3){
-                inventory.setItem(human, cover);
-                human = - 99;
+            } else {
+                if (random.nextDouble() < 0.7) {
+                    inventory.setItem(humanSlot, rule.getGameMode() == GameRules.GameMode.FULLINVENTORY? grass : null);
+                    humanSlot = -99;
+                }
             }
         }
     }
